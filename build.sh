5db32de510a51c501b5030a455256cce5ab3123b
Rahul Koneru <rahul9babu@gmail.com>
2020-05-20 17:31:40 -0400

    ls usr and lib

--- build.sh
+++ build.sh
@@ -5,19 +5,22 @@ set -ev
 uname -s
 uname -n
 
-#ls $TRAVIS_BUILD_DIR/openmpi/bin/
+ls $TRAVIS_BUILD_DIR/openmpi/bin/mpi*
 #echo "After ls"
 #sudo cp $TRAVIS_BUILD_DIR/openmpi/bin/mpi* /usr/bin/
 #echo "After cp"
+
+ls /usr/
+ls /usr/lib/*mpi*
 
 which gfortran
 which cpp
 #which mpif90
 #which mpic++
 
-make clean
-echo 'Cleaning directory'
-echo '--------------------------'
-echo 'Runnnig RFLU=1'
-make RFLU=1
+#make clean
+#echo 'Cleaning directory'
+#echo '--------------------------'
+#echo 'Runnnig RFLU=1'
+#make RFLU=1
 
