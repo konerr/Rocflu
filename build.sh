b1b28fcbdf3f1c9af9ad37f7e058f13b6b45592e
Rahul Koneru <rahul9babu@gmail.com>
2020-05-20 11:36:00 -0400

    no ls

--- build.sh
+++ build.sh
@@ -5,12 +5,10 @@ set -ev
 uname -s
 uname -n
 
-ls $TRAVIS_BUILD_DIR/openmpi/bin/
-ls /usr/bin/
-echo "After ls"
-sudo cp $TRAVIS_BUILD_DIR/openmpi/bin/mpi* /usr/bin/
-ls /usr/bin/
-echo "After cp"
+#ls $TRAVIS_BUILD_DIR/openmpi/bin/
+#echo "After ls"
+#sudo cp $TRAVIS_BUILD_DIR/openmpi/bin/mpi* /usr/bin/
+#echo "After cp"
 
 which gfortran
 which cpp
